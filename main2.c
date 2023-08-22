6ce91da52b68d2ec8445f3de0706cb2c3eec11bc
Arin Bansal <142030133+KiritoKageyama@users.noreply.github.com>
2023-08-23 00:37:09 +0530

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,38 @@
+#include<math.h>
+#include<stdio.h>
+int main(){
+    int num;
+    printf("enter an integer=","\n");
+    {if (num <= num)
+            scanf("%d",&num);
+    }
+// true  if num is perfectely divisible by 2//
+    {
+        {
+            if (num % 2 == 0)
+                printf("%d is even.", num);
+            else
+                printf("%d is odd.", num);
+        }
+        {
+            if (num == 0)
+                printf("%d is not defined.");
+            {printf("\ny =");
+                scanf("%d",&num);}
+        }
+        int i = 0;
+        if (num <= num) {
+            for (i = num; i <= (10);i++) {
+                printf("%d\n", i*=2,i);
+            }
+        }
+    }
+    {
+        printf_s("the ans is = ");
+        printf("%d\n", num=(num*(num+1)/2));
+    }
+
+
+    return 0;
+}
+
